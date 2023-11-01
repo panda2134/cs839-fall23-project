b1a4d67839a4cde20b559a05180f8ec9e1d7f16a
panda2134 <me@panda2134.site>
2023-10-31 23:24:48 -0500

    add vscoqtop LSP

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
+    vscoq.url = "github:coq-community/vscoq";
     flake-utils.follows = "nix-vscode-extensions/flake-utils";
     nixpkgs.follows = "nix-vscode-extensions/nixpkgs";
   };
@@ -15,14 +16,21 @@
 
           packages.default =
             vscode-with-extensions.override {
-              vscode = vscodium.fhsWithPackages (ps: [ ps.coq ]);
+              vscode = vscodium.fhsWithPackages (ps: [
+                ps.coq
+                inputs.vscoq.packages.${system}.vscoq-language-server
+              ]);
               vscodeExtensions = [
                 extensions.vscode-marketplace.maximedenes.vscoq
               ];
             };
 
           devShells.default = pkgs.mkShell {
-            buildInputs = [ packages.default pkgs.coq ];
+            buildInputs = [
+              packages.default
+              pkgs.coq
+              inputs.vscoq.packages.${system}.vscoq-language-server
+            ];
             shellHook = ''
               printf "VSCodium with extensions:\n"
               codium --list-extensions
