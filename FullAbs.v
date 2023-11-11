f0bc92b2598268b87a4de00fc72e05255a0e4e00
panda2134 <me@panda2134.site>
2023-11-11 16:33:56 -0600

    compiler correctness

--- FullAbs.v
+++ FullAbs.v
@@ -1,26 +1,172 @@
 Require Import ZArith.
-From stdpp Require Import prelude.
 Require Import Coq.Lists.List.
-Add LoadPath "/nix/store/4w62wnc2x4a7pmk8qrhy0jwsaw8p1zsr-coq8.17-stdpp-1.9.0/lib/coq/8.17/user-contrib/stdpp".
+Import ListNotations.
+Require Coq.Program.Wf.
+Require Import Recdef.
 
-Inductive Expr :=
+Inductive Expr: Type :=
     Num: Z -> Expr
   | Plus: Expr -> Expr -> Expr
   | Minus: Expr -> Expr -> Expr
-  | Arg: Expr
-  | Call: nat -> Z -> Expr. (* fn names are nats*)
+  | VarX: Expr
+  | VarY: Expr
+  | LetX: Expr -> Expr -> Expr
+  | LetY: Expr -> Expr -> Expr
+  | IteZero: Expr -> Expr -> Expr -> Expr
+  .
 
 
+(* cannot handle func call: recursiveness? *)
+(* I even don't know how to handle it when recursing is not allowed! *)
 
-Fixpoint evalExpr (e: Expr) (arg: Z) (globalFun: nat -> Expr): Z :=
+Fixpoint evalExpr (e: Expr) (varX: Z) (varY: Z): Z :=
   match e with
   | Num n => n
-  | Plus e1 e2 => (evalExpr e1 arg globalFun) + (evalExpr e2 arg globalFun)
-  | Minus e1 e2 => (evalExpr e1 arg globalFun) - (evalExpr e2 arg globalFun)
-  | Arg => arg
-  | Call fn x => evalExpr (globalFun fn) x ()
+  | Plus e1 e2 => (evalExpr e1 varX varY) + (evalExpr e2 varX varY)
+  | Minus e1 e2 => (evalExpr e1 varX varY) - (evalExpr e2 varX varY)
+  | VarX => varX
+  | VarY => varY
+  | LetX e1 e2 => let varX' := (evalExpr e1 varX varY) in (evalExpr e2 varX' varY)
+  | LetY e1 e2 => let varY' := (evalExpr e1 varX varY) in (evalExpr e2 varX varY')
+  | IteZero ec el er =>
+       let cond := (evalExpr ec varX varY) in
+       let l := (evalExpr el varX varY) in
+       let r := (evalExpr er varX varY) in
+       match cond with
+       | Z0 => l
+       | _ => r
+       end
   end.
 
-Lemma evalExprEx : (evalExpr (Plus (Num 3) Arg) 2) = Z.of_nat 5.
+Example E1: (Z.to_nat (evalExpr (Plus (Num 3) VarX) 2 3)) = 5.
 Proof.
   auto.
+Qed.
+
+Example E2: (Z.to_nat (evalExpr (IteZero (Minus (Num 1) (Num 1)) VarX VarY) 3 5)) = 3.
+Proof. unfold evalExpr. auto.
+Qed.
+
+Inductive Instr: Type := 
+    IPlus: Instr
+  | IMinus: Instr
+  | ICondPick: Instr (* Remove all 3 elements. If StackTop is Zero then Pick StackTop-1 else Pick StackTop-2 *)
+  | ILoadX: Instr    (* Pop *)
+  | ILoadY: Instr
+  | IPop: Instr
+  | IStoreX: Instr   (* Push *)
+  | IStoreY: Instr
+  | IStoreImm: Z -> Instr
+  | IDup: Instr
+  | ISwap: Instr.
+
+Fixpoint evalInstr (prog: (list Instr)) (stack: list Z) (regX: Z) (regY: Z): option (list Z) :=
+  match (prog, stack) with
+  | (nil, result) => Some(result)
+  | (IPlus :: prog', n1 :: n2 :: rest) => (evalInstr prog' ((n2 + n1)%Z :: rest) regX regY)
+  | (IMinus :: prog', n1 :: n2 :: rest) => (evalInstr prog' ((n2 - n1)%Z :: rest) regX regY)
+  | (ICondPick :: prog', cond :: l :: r :: rest) =>
+    match cond with
+    | Z0 => (evalInstr prog' (l :: rest) regX regY)
+    | _ => (evalInstr prog' (r :: rest) regX regY)
+    end
+  | (ILoadX :: prog', n :: rest) => (evalInstr prog' rest n regY)
+  | (ILoadY :: prog', n :: rest) => (evalInstr prog' rest regX n)
+  | (IStoreX :: prog', _) => (evalInstr prog' (regX :: stack) regX regY)
+  | (IStoreY :: prog', _) => (evalInstr prog' (regY :: stack) regX regY)
+  | ((IStoreImm imm) :: prog', _) => (evalInstr prog' (imm :: stack) regX regY)
+  | (IPop :: prog', n :: rest) => (evalInstr prog' rest regX regY)
+  | (IDup :: prog', n :: rest) => (evalInstr prog' (n :: n :: rest) regX regY)
+  | (ISwap :: prog', n1 :: n2 :: rest) => (evalInstr prog' (n2 :: n1 :: rest) regX regY)
+  | (_, _) => None
+  end.
+
+Example IE1: (evalInstr (IDup :: IPlus :: nil) (1%Z :: nil) 0 0) = Some(2%Z :: nil).
+Proof.
+  unfold evalInstr.
+  auto.
+Qed.
+
+Search Expr.
+
+Fixpoint compile (e: Expr): list Instr :=
+  match e with
+  | VarX => [IStoreX]
+  | VarY => [IStoreY]
+  | LetX e1 e2 =>
+    let e1Prog := (compile e1) in
+    let e2Prog := (compile e2) in
+    e1Prog ++ [IStoreX ; ISwap ; ILoadX] ++ e2Prog ++ [ISwap; ILoadX]
+  | LetY e1 e2 =>
+    let e1Prog := (compile e1) in
+    let e2Prog := (compile e2) in
+    e1Prog ++ [IStoreY ; ISwap ; ILoadY] ++ e2Prog ++ [ISwap; ILoadY]
+  | Num n => [(IStoreImm n)]
+  | Plus e1 e2 => 
+    let e1Prog := (compile e1) in
+    let e2Prog := (compile e2) in
+    e1Prog ++ e2Prog ++ [IPlus]
+  | Minus e1 e2 => 
+    let e1Prog := (compile e1) in
+    let e2Prog := (compile e2) in
+    e1Prog ++ e2Prog ++ [IMinus]
+  | IteZero ec el er =>
+    let elProg := (compile el) in
+    let erProg := (compile er) in
+    let ecProg := (compile ec) in
+    erProg ++ elProg ++ ecProg ++ [ICondPick]
+  end.
+
+Compute evalInstr (compile (LetX (Num 1) (LetX (Num 2) VarX))) [] 0 0.
+
+Function stackTop (xs: option (list Z)): option Z :=
+  match xs with
+  | None => None
+  | Some(nil) => None
+  | Some(x :: rest) => Some(x)
+  end.
+
+Lemma step_correct: forall e: Expr, forall x0 y0: Z, forall progs: list Instr, forall st: list Z,
+  (evalInstr ((compile e) ++ progs) st x0 y0) = (evalInstr progs ((evalExpr e x0 y0) :: st) x0 y0).
+Proof.
+  induction e.
+  - (*Num*) auto.
+  - (*Plus*) simpl.
+    intros x0 y0 progs st. repeat rewrite <- app_assoc.
+    rewrite IHe1. rewrite IHe2. simpl. reflexivity.
+  - (*Minus*) simpl.
+    intros x0 y0 progs st. repeat rewrite <- app_assoc.
+    rewrite IHe1. rewrite IHe2. simpl. reflexivity.
+  - (*VarX*) auto.
+  - (*VarY*) auto.
+  - (*LetX*) simpl.
+    intros x0 y0 progs st.
+    repeat rewrite <- app_assoc.
+    rewrite IHe1. simpl.
+    repeat rewrite <- app_assoc.
+    rewrite IHe2. reflexivity.
+  - (*LetY*) simpl.
+    intros x0 y0 progs st.
+    repeat rewrite <- app_assoc.
+    rewrite IHe1. simpl.
+    repeat rewrite <- app_assoc.
+    rewrite IHe2. reflexivity.
+  - (*IteZero*) intros x0 y0 progs st.
+    simpl (compile (IteZero e1 e2 e3) ++ progs). repeat rewrite <- app_assoc. rewrite IHe3. rewrite IHe2. rewrite IHe1.
+    simpl (evalInstr ([ICondPick] ++ progs) (evalExpr e1 x0 y0 :: evalExpr e2 x0 y0 :: evalExpr e3 x0 y0 :: st) x0 y0).
+    destruct (evalExpr e1 x0 y0) eqn:condLeft.
+    all: try simpl (evalInstr progs (evalExpr (IteZero e1 e2 e3) x0 y0 :: st) x0 y0).
+    * rewrite -> condLeft. try rewrite Z.mul_0_l. simpl. destruct (evalExpr e2 x0 y0). all:repeat auto.
+    * rewrite -> condLeft. try rewrite Z.mul_0_l. simpl. destruct (evalExpr e3 x0 y0). all:reflexivity.
+    * simpl. destruct (evalExpr e1 x0 y0).
+      + discriminate. + reflexivity. + reflexivity.
+Qed.
+
+Theorem compiler_correct: forall e: Expr, forall x0 y0: Z,
+  Some(evalExpr e x0 y0) = stackTop (evalInstr (compile e) [] x0 y0).
+Proof.
+  assert (forall e: Expr, forall x0 y0: Z,
+      evalInstr (compile e ++ []) [] x0 y0 = evalInstr (compile e) [] x0 y0).
+  - intros. rewrite app_nil_r. reflexivity.
+  - intros. rewrite <- H. rewrite step_correct. reflexivity.
+Qed.
--- flake.nix
+++ flake.nix
@@ -1,17 +1,7 @@
 {
   inputs = {
-    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
-    vscoq.url = "github:coq-community/vscoq";
-    flake-utils.follows = "nix-vscode-extensions/flake-utils";
-    nixpkgs.follows = "nix-vscode-extensions/nixpkgs";
-    coq-8_18 = {
-      type = "github";
-      owner = "coq";
-      repo = "coq";
-      ref = "V8.18.0";
-    };
-
-    coq-8_18.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = inputs:
@@ -19,32 +9,17 @@
       (system:
         let
           pkgs = inputs.nixpkgs.legacyPackages.${system};
-          extensions = inputs.nix-vscode-extensions.extensions.${system};
-          inherit (pkgs) vscode-with-extensions vscodium;
-
-          packages.default =
-            vscode-with-extensions.override {
-              vscode = vscodium;
-              vscodeExtensions = [
-                extensions.vscode-marketplace.maximedenes.vscoq
-              ];
-            };
 
           devShells.default = pkgs.mkShell {
             buildInputs = [
-              packages.default
-              inputs.coq-8_18.defaultPackage.${system}
-              inputs.vscoq.packages.${system}.vscoq-language-server
-              (inputs.nixpkgs.legacyPackages.${system}.coqPackages.callPackage ./stdpp.nix {})
+              pkgs.coq
+              pkgs.coqPackages.stdpp
+              pkgs.coqPackages.coqide
             ];
-            shellHook = ''
-              printf "VSCodium with extensions:\n"
-              codium --list-extensions
-            '';
           };
         in
         {
-          inherit packages devShells;
+          inherit devShells;
         });
 }
 
