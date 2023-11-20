84c3f8b729dbafb16a734434f84b257342509df7
panda2134 <me@panda2134.site>
2023-11-20 13:37:11 -0600

    attempting on equiv preservation

--- FullAbs.v
+++ FullAbs.v
@@ -79,13 +79,13 @@ Inductive Instr: Type :=
   | ISwap: Instr.
 
 (* return: stack & write events *)
-Fixpoint evalInstr (prog: (list Instr)) (stack: list Z) (regX: Z) (regY: Z): option (list Z * list Z) :=
+Fixpoint evalInstr (prog: (list Instr)) (stack: list Z) (regX: Z) (regY: Z): option (list Z * list Z * (Z*Z)) :=
   match (prog, stack) with
-  | (nil, result) => Some(result, [])
+  | (nil, result) => Some(result, [], (regX, regY))
   | (IPlus :: prog', n1 :: n2 :: rest) => (evalInstr prog' ((n2 + n1)%Z :: rest) regX regY)
   | (IMinus :: prog', n1 :: n2 :: rest) => (evalInstr prog' ((n2 - n1)%Z :: rest) regX regY)
   | (IWrite :: prog', n :: rest) => match (evalInstr prog' stack regX regY) with
-                                    | Some(stack', evt') => Some(stack', n :: evt')
+                                    | Some(stack', evt', (regX', regY')) => Some(stack', n :: evt', (regX', regY'))
                                     | None => None
                                     end
   | (ICondPick :: prog', cond :: l :: r :: rest) =>
@@ -104,62 +104,62 @@ Fixpoint evalInstr (prog: (list Instr)) (stack: list Z) (regX: Z) (regY: Z): opt
   | (_, _) => None
   end.
 
-Example InstrE1: (evalInstr (IDup :: IPlus :: nil) (1%Z :: nil) 0 0) = Some([2%Z], []).
+Example InstrE1: (evalInstr (IDup :: IPlus :: nil) (1%Z :: nil) 0 0) = Some([2%Z], [], (0%Z,0%Z)).
 Proof.
   unfold evalInstr.
   auto.
 Qed.
 
-Example InstrE2: (evalInstr (IStoreX :: IWrite :: IPop :: nil) [] 3%Z 4%Z) = Some([], [3%Z]).
+Example InstrE2: (evalInstr (IStoreX :: IWrite :: IPop :: nil) [] 3%Z 4%Z) = Some([], [3%Z], (3%Z, 4%Z)).
 Proof.
   auto.
 Qed.
 
-Fixpoint compileExpr (e: Expr): list Instr :=
+Fixpoint compile_expr (e: Expr): list Instr :=
   match e with
   | VarX => [IStoreX]
   | VarY => [IStoreY]
   | Num n => [(IStoreImm n)]
   | Plus e1 e2 => 
-    let e1Prog := (compileExpr e1) in
-    let e2Prog := (compileExpr e2) in
+    let e1Prog := (compile_expr e1) in
+    let e2Prog := (compile_expr e2) in
     e1Prog ++ e2Prog ++ [IPlus]
   | Minus e1 e2 => 
-    let e1Prog := (compileExpr e1) in
-    let e2Prog := (compileExpr e2) in
+    let e1Prog := (compile_expr e1) in
+    let e2Prog := (compile_expr e2) in
     e1Prog ++ e2Prog ++ [IMinus]
   | IteZero ec el er =>
-    let elProg := (compileExpr el) in
-    let erProg := (compileExpr er) in
-    let ecProg := (compileExpr ec) in
+    let elProg := (compile_expr el) in
+    let erProg := (compile_expr er) in
+    let ecProg := (compile_expr ec) in
     erProg ++ elProg ++ ecProg ++ [ICondPick]
   end.
 
-Fixpoint compileStmt (s: Stmt): list Instr :=
+Fixpoint compile_stmt (s: Stmt): list Instr :=
   match s with
   | LetX e1 e2 =>
-    let e1Prog := (compileExpr e1) in
-    let e2Prog := (compileStmt e2) in
+    let e1Prog := (compile_expr e1) in
+    let e2Prog := (compile_stmt e2) in
     e1Prog ++ [IStoreX ; ISwap ; ILoadX] ++ e2Prog ++ [ILoadX]
   | LetY e1 e2 =>
-    let e1Prog := (compileExpr e1) in
-    let e2Prog := (compileStmt e2) in
+    let e1Prog := (compile_expr e1) in
+    let e2Prog := (compile_stmt e2) in
     e1Prog ++ [IStoreY ; ISwap ; ILoadY] ++ e2Prog ++ [ILoadY]
-  | Write e => (compileExpr e) ++ [IWrite; IPop]
+  | Write e => (compile_expr e) ++ [IWrite; IPop]
   | Seq e1 e2 =>
-    let e1Prog := (compileStmt e1) in
-    let e2Prog := (compileStmt e2) in
+    let e1Prog := (compile_stmt e1) in
+    let e2Prog := (compile_stmt e2) in
     e1Prog ++ e2Prog
   end.
 
-Compute compileStmt (LetX (Num 1) (Write VarX)).
+Compute compile_stmt (LetX (Num 1) (Write VarX)).
 
-Example CE1: evalInstr (compileStmt (LetX (Num 1) (LetX (Num 2) (Write VarX)))) [] 0 0 = Some([], [2%Z]).
+Example CE1: evalInstr (compile_stmt (LetX (Num 1) (LetX (Num 2) (Write VarX)))) [] 0 0 = Some([], [2%Z], (0%Z, 0%Z)).
 Proof. simpl. reflexivity.
 Qed.
 
 Lemma step_correct_expr: forall e: Expr, forall x0 y0: Z, forall progs: list Instr, forall st: list Z,
-  (evalInstr ((compileExpr e) ++ progs) st x0 y0) = (evalInstr progs ((evalExpr e x0 y0) :: st) x0 y0).
+  (evalInstr ((compile_expr e) ++ progs) st x0 y0) = (evalInstr progs ((evalExpr e x0 y0) :: st) x0 y0).
 Proof.
   induction e.
   - (*Num*) auto.
@@ -172,7 +172,7 @@ Proof.
   - (*VarX*) auto.
   - (*VarY*) auto.
   - (*IteZero*) intros x0 y0 progs st.
-    simpl (compileExpr (IteZero e1 e2 e3) ++ progs). repeat rewrite <- app_assoc. rewrite IHe3. rewrite IHe2. rewrite IHe1.
+    simpl (compile_expr (IteZero e1 e2 e3) ++ progs). repeat rewrite <- app_assoc. rewrite IHe3. rewrite IHe2. rewrite IHe1.
     simpl (evalInstr ([ICondPick] ++ progs) (evalExpr e1 x0 y0 :: evalExpr e2 x0 y0 :: evalExpr e3 x0 y0 :: st) x0 y0).
     destruct (evalExpr e1 x0 y0) eqn:condLeft.
     all: try simpl (evalInstr progs (evalExpr (IteZero e1 e2 e3) x0 y0 :: st) x0 y0).
@@ -183,16 +183,16 @@ Proof.
 Qed.
 
 Lemma step_correct_stmt: forall s: Stmt, forall x0 y0: Z, forall progs: list Instr, forall st: list Z,
-  let e1 := (evalInstr ((compileStmt s) ++ progs) st x0 y0) in
+  let e1 := (evalInstr ((compile_stmt s) ++ progs) st x0 y0) in
   let evt0 := (evalStmt s x0 y0) in
   let e2 := (evalInstr progs st x0 y0) in
   let l := match e1 with
            | None => None
-           | Some(_, evt) => Some(evt)
+           | Some(_, evt, _) => Some(evt)
            end in
   let r := match e2 with
            | None => None
-           | Some(_, evt) => Some(evt0 ++ evt)
+           | Some(_, evt, _) => Some(evt0 ++ evt)
            end in
   l = r.
 Proof.
@@ -209,33 +209,35 @@ Proof.
     reflexivity.
   - (*Write*) simpl.
     intros x0 y0 progs st.
-    assert (((compileExpr e ++ [IWrite; IPop]) ++ progs) = (compileExpr e ++ [IWrite; IPop] ++ progs)) as H.
+    assert (((compile_expr e ++ [IWrite; IPop]) ++ progs) = (compile_expr e ++ [IWrite; IPop] ++ progs)) as H.
     + rewrite app_assoc. reflexivity.
     + rewrite H. rewrite step_correct_expr. simpl. destruct (evalInstr progs st x0 y0) as [p|].
-      * destruct p. reflexivity.
+      * destruct p. destruct p. destruct p0. reflexivity.
       * reflexivity.
   - (*Seq*) simpl in IHs1. simpl in IHs2. intros x0 y0 progs st. simpl.
-    rewrite <- app_assoc. rewrite IHs1. destruct_with_eqn (evalInstr (compileStmt s2 ++ progs) st x0 y0).
+    rewrite <- app_assoc. rewrite IHs1. destruct_with_eqn (evalInstr (compile_stmt s2 ++ progs) st x0 y0).
     destruct_with_eqn (evalInstr progs st x0 y0).
     + specialize (IHs2 x0 y0 progs st). rewrite Heqo in IHs2. rewrite Heqo0 in IHs2.
-      destruct_with_eqn p. destruct_with_eqn p0. rewrite <- app_assoc. inversion IHs2.
+      destruct_with_eqn p. destruct_with_eqn p0. destruct_with_eqn p1. destruct_with_eqn p3. rewrite <- app_assoc. inversion IHs2.
       reflexivity.
-    + destruct p. exfalso. specialize (IHs2 x0 y0 progs st). rewrite Heqo in IHs2. rewrite Heqo0 in IHs2. inversion IHs2.
+    + destruct p. exfalso. specialize (IHs2 x0 y0 progs st). rewrite Heqo in IHs2. rewrite Heqo0 in IHs2. destruct p in IHs2. inversion IHs2.
     + specialize (IHs2 x0 y0 progs st). rewrite Heqo in IHs2. destruct_with_eqn (evalInstr progs st x0 y0).
-      ++ destruct_with_eqn p. exfalso. inversion IHs2.
+      ++ destruct_with_eqn p. exfalso. destruct p0 in IHs2. inversion IHs2.
       ++ reflexivity.
 Qed.
 
-Theorem compiler_correct: forall s: Stmt, forall x0 y0: Z, exists st': list Z,
-  evalInstr (compileStmt s) [] x0 y0 = Some(st', evalStmt s x0 y0).
+Theorem compiler_correct: forall s: Stmt, forall x0 y0: Z, exists st': list Z, exists x0' y0': Z,
+  evalInstr (compile_stmt s) [] x0 y0 = Some(st', evalStmt s x0 y0, (x0', y0')).
 Proof.
   assert (forall s: Stmt, forall x0 y0: Z,
-      evalInstr (compileStmt s ++ []) [] x0 y0 = evalInstr (compileStmt s) [] x0 y0).
+      evalInstr (compile_stmt s ++ []) [] x0 y0 = evalInstr (compile_stmt s) [] x0 y0).
   - intros. rewrite app_nil_r. reflexivity.
-  - intros. rewrite <- H. destruct_with_eqn (evalInstr (compileStmt s ++ []) [] x0 y0).
-    + destruct p eqn:Hp. pose proof (step_correct_stmt s x0 y0 [] []) as HC. rewrite Heqo in HC. simpl in HC. rewrite app_nil_r in HC.
-      inversion HC. exists l. rewrite <- H1. reflexivity.
-    + exfalso. destruct (evalInstr (compileStmt s ++ []) [] x0 y0) eqn:? in Heqo.
+  - intros. rewrite <- H. destruct_with_eqn (evalInstr (compile_stmt s ++ []) [] x0 y0).
+    + destruct p eqn:Hp. pose proof (step_correct_stmt s x0 y0 [] []) as HC. rewrite Heqo in HC. simpl in HC.
+      revert HC. destruct p0 eqn:HCp0. intros.
+      rewrite app_nil_r in HC.
+      inversion HC. rewrite <- H1. exists l. destruct p1. exists z. exists z0. reflexivity.
+    + exfalso. destruct (evalInstr (compile_stmt s ++ []) [] x0 y0) eqn:? in Heqo.
       ++ inversion Heqo.
       ++ pose proof (step_correct_stmt s x0 y0 [] []) as HC. rewrite Heqo0 in HC. simpl in HC. inversion HC.
 Qed.
@@ -261,8 +263,8 @@ Fixpoint link_to_context (s: Stmt) (ctx: StmtContext): Stmt :=
   | CSeqR s' ctx' => Seq s' (link_to_context s ctx')
   end.
 
-Example LE1: (evalInstr (compileStmt (link_to_context (Write VarX) (CLetX (Num 1) (CSeqR (Write VarX) (CLetX (Num 2) CHole))))) [] 99%Z 99%Z)
-           = Some([], [1%Z; 2%Z]).
+Example LE1: (evalInstr (compile_stmt (link_to_context (Write VarX) (CLetX (Num 1) (CSeqR (Write VarX) (CLetX (Num 2) CHole))))) [] 99%Z 99%Z)
+           = Some([], [1%Z; 2%Z], (99%Z, 99%Z)).
 Proof. auto.
 Qed.
 
@@ -276,7 +278,7 @@ Definition context_equivalence_stmt (s1 s2: Stmt): Prop :=
 
 (* Part 1 of full abstraction *)
 Theorem equivalence_reflection: forall p1 p2: list Instr, forall s1 s2: Stmt,
-  (compileStmt s1 = p1) /\ (compileStmt s2 = p2) /\ (context_equivalence_instr p1 p2) -> (context_equivalence_stmt s1 s2).
+  (compile_stmt s1 = p1) /\ (compile_stmt s2 = p2) /\ (context_equivalence_instr p1 p2) -> (context_equivalence_stmt s1 s2).
 Proof.
   intros. destruct H as [H1]. destruct H as [H2].
   unfold context_equivalence_instr in H.
@@ -295,8 +297,9 @@ Proof.
 
   induction ctx.
   + intros. unfold link_to_context.
-    pose proof (compiler_correct s1 x y) as HC1. destruct HC1 as [st1]. rewrite H1 in H0.
-    pose proof (compiler_correct s2 x y) as HC2. destruct HC2 as [st2]. rewrite H2 in H3.
+    pose proof (compiler_correct s1 x y) as HC1. destruct HC1 as [st1 [x0 [y0 H0]]].
+    rewrite H1 in H0.
+    pose proof (compiler_correct s2 x y) as HC2. destruct HC2 as [st2 [x0' [y0' H3]]]. rewrite H2 in H3.
     assert (forall p: list Instr, p = [] ++ p ++ []).
     intros. rewrite app_nil_r. rewrite app_nil_l. reflexivity.
 
@@ -311,15 +314,64 @@ Proof.
 Qed.
   (* 
 Fixpoint context_back_translation (c1 c2: list Instr) (st: list Z) (x y: Z) -> StmtContext :=
-  
-*)
+   *)
+
+Lemma prefix_removal: forall p1 p2 c1: list Instr,
+  (forall st: list Z, forall x y: Z, evalInstr p1 st x y = evalInstr p2 st x y) -> 
+  (forall st': list Z, forall x' y': Z, evalInstr (c1 ++ p1) st' x' y' = evalInstr (c1 ++ p2) st' x' y').
+Proof.
+  intros p1 p2 c1. intros IH0.
+  induction c1.
+  + repeat rewrite app_nil_l. easy.
+  + repeat rewrite <- app_comm_cons. destruct a. all: try (repeat simpl; intros; rewrite IHc1; easy).
+    - simpl. destruct st'. easy. destruct st'. easy. apply IHc1.
+    - simpl. destruct st'. easy. destruct st'. easy. apply IHc1.
+    - simpl. destruct st'. easy. destruct st'. easy. destruct st'. easy. destruct z.
+      all: apply IHc1. 
+    - simpl. destruct st'. easy. intros. rewrite IHc1. easy.
+    - simpl. destruct st'. easy. intros. apply IHc1.
+    - simpl. intros. destruct st'. easy. apply IHc1.
+    - simpl. intros. destruct st'. easy. rewrite IHc1. easy.
+    - simpl. intros. destruct st'. easy. destruct st'. easy. rewrite IHc1. easy.
+Qed.
+
+>>>>>>> 58c17dc (attempting on equiv preservation)
 (* Part 2 of full abstraction *)
 Theorem equivalence_preservation: forall p1 p2: list Instr, forall s1 s2: Stmt,
-  (compileStmt s1 = p1) /\ (compileStmt s2 = p2) /\ (context_equivalence_stmt s1 s2) -> (context_equivalence_instr p1 p2).
+  (compile_stmt s1 = p1) /\ (compile_stmt s2 = p2) /\ (context_equivalence_stmt s1 s2) -> (context_equivalence_instr p1 p2).
 Proof.
-  intros. destruct H as [H1]. destruct H as [H2].
+  intros. destruct H as [H1 [H H2]].
   unfold context_equivalence_stmt in H.
+<<<<<<< HEAD
   unfold context_equivalence_instr.
   (* TODO: This cannot be proven because it's false!
            A counterexample (where c1 causes stack underflow) actually exists! *)
 Abort.
+=======
+  unfold context_equivalence_instr. intros c1 c2 st.
+  apply prefix_removal. intros. unfold context_equivalence_stmt in H2.
+  pose proof (H2 CHole x y) as H3. unfold link_to_context in H3.
+  pose proof (compiler_correct s1 x y) as Hs1. pose proof (compiler_correct s2 x y) as Hs2.
+  rewrite H1 in Hs1. rewrite H in Hs2. 
+Abort.
+
+Lemma stack_bot_elim: forall p: list Instr, forall x y x1 y1 x2 y2: Z, forall st0 st1 st2 o1 o2: list Z,
+  (evalInstr p [] x y = Some(st1, o1, (x1, y1))) /\ (evalInstr p st0 x y = Some (st2, o2, (x2, y2)))
+  -> (o1 = o2).
+Proof.
+  induction p.
+  - intros. destruct H as [H1 H2]. simpl in H1. simpl in H2. inversion H1. inversion H2. easy.
+  - intros. destruct H as [H1 H2]. destruct a.
+    * try (simpl in H1; inversion H1; exfalso; assumption). (* IPlus *)
+    * try (simpl in H1; inversion H1; exfalso; assumption). (* IMinus *)
+    * try (simpl in H1; inversion H1; exfalso; assumption). (* ICondPick *)
+    * try (simpl in H1; inversion H1; exfalso; assumption). (* IWrite *)
+    * try (simpl in H1; inversion H1; exfalso; assumption). (* ILoadX *)
+    * try (simpl in H1; inversion H1; exfalso; assumption). (* ILoadY *)
+    * try (simpl in H1; inversion H1; exfalso; assumption). (* IPop *)
+    * simpl in H1. simpl in H2. (* unfold evalInstr in H1. *)
+Abort.
+
+
+
+
